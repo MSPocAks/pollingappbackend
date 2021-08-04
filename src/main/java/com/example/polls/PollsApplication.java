a34295e88b4e8f3c83fabd312e652e6cde7c1e32
satish sharma <satish sharma>
2021-08-04 17:17:36 +0530

    initial code

---
+++ src/main/java/com/example/polls/PollsApplication.java
@@ -0,0 +1,29 @@
+package com.example.polls;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+@EnableSwagger2
+@SpringBootApplication
+@EntityScan(basePackageClasses = {
+		PollsApplication.class,
+		Jsr310JpaConverters.class
+})
+public class PollsApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollsApplication.class, args);
+	}
+}
